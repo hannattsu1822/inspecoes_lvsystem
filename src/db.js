98594c04e1decc809cb5d34f12c479a698721631
hannattsu1822 <158786546+hannattsu1822@users.noreply.github.com>
2025-03-24 22:11:36 -0300

    db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,14 @@
-const { Pool } = require('pg');
+const mysql = require('mysql2/promise'); // Usando mysql2 com suporte a async/await
 
-const pool = new Pool({
-  user: 'root',
-  host: 'localhost',
-  database: 'banco_linha',
-  password: '289956Hg@#',
-  port: 33006,
+const pool = mysql.createPool({
+  host: 'hopper.proxy.rlwy.net',      // Host do Railway
+  user: 'root',                       // Usuário (mesmo da URL)
+  password: 'CfYJeRDGchmxLVXDXQLhLWbByRkjRQPn', // Senha da URL
+  database: 'railway',                // Nome do banco (final da URL)
+  port: 28603,                        // Porta da URL
+  waitForConnections: true,           // Configurações opcionais
+  connectionLimit: 10,                // Limite de conexões
+  queueLimit: 0,
 });
 
 module.exports = pool;
